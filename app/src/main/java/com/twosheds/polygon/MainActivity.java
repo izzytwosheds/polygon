ca30361daf84d59c92111cf4110b13d027fe8580
Izzat Bahadirov <izzatb@sta.samsung.com>
2015-11-16 10:52:21 -0500

    Added a spinner

--- app/src/main/java/com/twosheds/polygon/MainActivity.java
+++ app/src/main/java/com/twosheds/polygon/MainActivity.java
@@ -2,6 +2,8 @@ package com.twosheds.polygon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_vertices);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.vertices_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
