810badb407cefc000b1183b917ea96f640c70641
Izzat Bahadirov <izzatb@sta.samsung.com>
2015-11-16 16:42:22 -0500

    Changing rotation angle based on horizontal drag gesture.

--- app/src/main/java/com/twosheds/polygon/PolygonView.java
+++ app/src/main/java/com/twosheds/polygon/PolygonView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -14,10 +15,11 @@ public class PolygonView extends View {
     private int mCenterX;
     private int mCenterY;
     private float mRadius;
+    private Paint mPaint;
 
     private int mNumSides;
 
-    private Paint mPaint;
+    private double mAngle;
 
     public PolygonView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -26,6 +28,8 @@ public class PolygonView extends View {
         mPaint = new Paint();
         mPaint.setColor(Color.RED);
         mPaint.setStrokeWidth(6.0f);
+
+        mAngle = 0;
     }
 
     @Override
@@ -40,15 +44,34 @@ public class PolygonView extends View {
         super.onDraw(canvas);
 
         for (int i=1; i<mNumSides; i++) {
-            double angle = (2 * Math.PI) / (mNumSides);
+            double delta = (2 * Math.PI) / (mNumSides);
 
-            float x1 = (float) (mCenterX + mRadius * Math.cos((i-1) * angle));
-            float y1 = (float) (mCenterY + mRadius * Math.sin((i - 1) * angle));
-            float x2 = (float) (mCenterX + mRadius * Math.cos(i * angle));
-            float y2 = (float) (mCenterY + mRadius * Math.sin(i * angle));
+            float x1 = (float) (mCenterX + mRadius * Math.cos(mAngle + (i-1) * delta));
+            float y1 = (float) (mCenterY + mRadius * Math.sin(mAngle + (i-1) * delta));
+            float x2 = (float) (mCenterX + mRadius * Math.cos(mAngle + i * delta));
+            float y2 = (float) (mCenterY + mRadius * Math.sin(mAngle + i * delta));
 
             canvas.drawLine(x1, y1, x2, y2, mPaint);
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                if (event.getHistorySize() > 1) {
+                    float deltaX = event.getX() - event.getHistoricalX(1);
+                    double alpha = deltaX * 2 * Math.PI / mCenterX;
+
+                    mAngle += alpha;
+
+                    postInvalidate();
+                }
+                return true;
+        }
+        return super.onTouchEvent(event);
     }
 
     void setNumSides(int numSides) {
